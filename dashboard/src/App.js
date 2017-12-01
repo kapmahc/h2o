be6ce25026f22fe1686c7ec62278d8424013a54b
Jeremy Zheng <jitang.zheng@gmail.com>
2017-11-30 16:31:50 -0800

    install packages

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,20 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Exception from 'ant-design-pro/lib/Exception';
+
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return (<div>
+      <Exception type="404"/>
+    </div>);
   }
 }
 
